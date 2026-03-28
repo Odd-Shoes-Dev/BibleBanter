9b17ba836fb27ebe50071724fbf39ae8d2a09cf8
shadrack-ss <shadrackssenkaayi21@gmail.com>
2026-03-28 18:28:27 +0300

    Use VITE_BACKEND_URL env var for production socket connection

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
-const URL = import.meta.env.DEV ? 'http://localhost:3001' : window.location.origin;
+const URL = import.meta.env.VITE_BACKEND_URL || (import.meta.env.DEV ? 'http://localhost:3001' : window.location.origin);
 
 export const socket = io(URL, {
   autoConnect: false,
